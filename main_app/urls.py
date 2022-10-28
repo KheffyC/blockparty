9f0235bbf1f17ad5edd61ffded3628ac4ac25c3f
chris-gallegos <chrisgallegos101@gmail.com>
2022-10-28 15:17:35 -0700

    added profile index and detail pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('global/<int:post_id>/', views.global_post_detail, name='global_post_detail'),
     path('global/create/', views.GlobalPostCreate.as_view(), name='global_post_create'),
     path('global/<int:pk>/update/', views.GlobalPostUpdate.as_view(), name='global_post_update'),
-    path('global/<int:pk>/delete/', views.GlobalPostDelete.as_view(), name='global_post_delete')
+    path('global/<int:pk>/delete/', views.GlobalPostDelete.as_view(), name='global_post_delete'),
+    path('profiles/', views.profiles_index, name='profiles_index'),
+    path('profiles/<int:user_id>/', views.profiles_detail, name='profiles_detail')
 ]
