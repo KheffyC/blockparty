d00646ff87fb83d3ec3f16af59cd20d9374bcd0a
KheffyC <kheffy.cervantez@gmail.com>
2022-10-28 13:55:57 -0700

    extend Profile model from User

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
+    # path('global/profile/create', views.ProfileCreate.as_view(), name='profile_create'),
     path('global/', views.global_index, name='global_index'),
     path('global/<int:post_id>/', views.global_post_detail, name='global_post_detail'),
     path('global/create/', views.GlobalPostCreate.as_view(), name='global_post_create'),
